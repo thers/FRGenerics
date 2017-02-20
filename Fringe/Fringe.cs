0fabdd4f42f3080434331d47b279ff16b49ae3b2
Alexander Kukhta <alexridersx@gmail.com>
2017-02-21 01:15:20 +0300

    Expiriments

--- Fringe/Fringe.cs
+++ Fringe/Fringe.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Drawing;
 using CitizenFX.Core;
 using CitizenFX.Core.UI;
@@ -6,11 +7,14 @@ using CitizenFX.Core.Native;
 
 namespace FRGenerics.Fringe {
   public class Fringe: BaseScript {
-    protected Text InteriorIndicator = new Text("Interior: nope", new PointF(10f, 10f), .7f);
+    protected Text InteriorIndicator = new Text("Interior: nope", new PointF(10f, 10f), .4f);
+    protected Text RT = new Text("RAY hit: nope", new PointF(10f, 40f), .4f);
 
     public Fringe() {
       Tick += OnTick;
     }
+    
+    protected Camera ncam;
 
     public async Task OnTick() {
       Player me = LocalPlayer;
@@ -35,19 +39,92 @@ namespace FRGenerics.Fringe {
 
       Function.Call(Hash._SET_RADAR_ZOOM_LEVEL_THIS_FRAME, 0);
       InteriorIndicator.Draw();
+      RT.Draw();
 
       /// Fucking hell...
       if (Game.IsControlJustReleased(0, Control.PhoneUp)) {
-        var g6_p1 = new Vector3(193.9493f, -1004.425f, -99.99999f);
+        //var g6_p1 = new Vector3(193.9493f, -1004.425f, -99.99999f);
+        //var g10_p1 = new Vector3(223.4f, -1001f, -100f);
 
-        var g10_p1 = new Vector3(223.4f, -1001f, -100f);
+        //Game.PlayerPed.Position = g10_p1;
+        //playerCam = World.RenderingCamera;
+        //Function.Call(Hash.LOAD_SCENE, -834.79f, 289.09f, 95.895f);
 
-        Game.PlayerPed.Position = g10_p1;
+        ncam = World.CreateCamera(new Vector3(-834.79f, 289.09f, 95.895f), Vector3.Zero, 60f);
+        ncam.PointAt(LocalPlayer.Character);
+
+        World.RenderingCamera.InterpTo(ncam, 10000, true, true);
+        //World.RenderingCamera = ncam;
       }
 
       if (Game.IsControlJustReleased(0, Control.PhoneDown)) {
-        Game.PlayerPed.Position = new Vector3(0f, 0f, 70f);
+        Debug.WriteLine("Whoa");
+        //Game.PlayerPed.Position = new Vector3(0f, 0f, 70f);
+
+        if (ncam != null) {
+          ncam.Delete();
+          ncam = null;
+        }
+
+        var wtf1 = new OutputArgument();
+        var wtf2 = new OutputArgument();
+
+        Function.Call(Hash.GET_SCREEN_RESOLUTION, wtf1, wtf2);
+
+        Debug.WriteLine(wtf1.GetResult<int>() + ":" + wtf2.GetResult<int>());
+
+        World.RenderingCamera = null;
       }
+
+      Vector3 pt = Game.PlayerPed.GetOffsetPosition(new Vector3(0, 20f, 0f));
+      
+      Function.Call(
+        Hash.DRAW_LINE,
+        Game.PlayerPed.Position.X, Game.PlayerPed.Position.Y, Game.PlayerPed.Position.Z,
+        pt.X, pt.Y, pt.Z,
+        255, 255, 0, 255
+      );
+
+      int rt = Function.Call<int>(
+        (Hash) 0x377906D8A31E5586,
+        Game.PlayerPed.Position.X, Game.PlayerPed.Position.Y, Game.PlayerPed.Position.Z,
+        pt.X, pt.Y, pt.Z,
+        1 | 2 | 16,
+        //1 | 2 | 4 | 8 | 16 | 32 | 64 | 128 | 256,
+        Game.PlayerPed,
+        0
+      );
+
+      var hit = new OutputArgument();
+      var endCoord = new OutputArgument();
+      var surfaceNormal = new OutputArgument();
+      var entityHit = new OutputArgument();
+
+      Function.Call(Hash.GET_SHAPE_TEST_RESULT, rt, hit, endCoord, surfaceNormal, entityHit);
+
+      bool hasHit = hit.GetResult<bool>();
+      int entity = entityHit.GetResult<int>();
+      Vector3 end = endCoord.GetResult<Vector3>();
+
+      if (hasHit) {
+        RT.Caption = "RAY hit: " +
+          (hasHit ? "yes" : "no") + ", " +
+          (hasHit ? (endCoord.GetResult<Vector3>().ToString()) : "") + ", " +
+          (new Vehicle(entity)).DisplayName.ToString();
+
+        Function.Call(
+          Hash.DRAW_LINE,
+          Game.PlayerPed.Position.X, Game.PlayerPed.Position.Y, Game.PlayerPed.Position.Z,
+          end.X, end.Y, end.Z,
+          255, 0, 255, 255
+        );
+      } else {
+        RT.Caption = "RAY hit: none";
+      }
+
+      //World.RenderingCamera.PointAt(LocalPlayer.Character);
+      //Debug.WriteLine("Cam active: " + (World.RenderingCamera.IsActive ? "yes" : "no"));
+      //Debug.WriteLine("Cam exist: " + (World.RenderingCamera.Exists() ? "yes" : "no"));
 
       await Task.FromResult(0);
     }
